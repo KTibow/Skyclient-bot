9a122b34e7944e978397704bcf37899932476362
Kendell R <KTibow@users.noreply.github.com>
2022-05-01 18:20:50 -0700

    just a little autoformatting

--- src/commands/shudup.ts
+++ src/commands/shudup.ts
@@ -8,32 +8,29 @@ export default class shudup extends BotCommand {
 		super('up', {
 			aliases: ['up'],
 			description: 'shud up',
-            args: [
-                {id: 'duration', type: 'string'},
-                { id: 'person', type: 'user', match: 'restContent' }
-        ],
-            prefix: 'shut ',
+			args: [
+				{ id: 'duration', type: 'string' },
+				{ id: 'person', type: 'user', match: 'restContent' },
+			],
+			prefix: 'shut ',
 			slash: false,
 		})
 	}
 	async exec(message, args) {
-        if (message.author.id != "435443705055543306") 
-            return;
+		if (message.author.id != '435443705055543306') return
 
-        if (args.person != null) {
-            console.log(args.person);
-            const badguy = await message.guild.members.fetch(args.person.id);
-            let time = 1000; // ms
-            if (args.duration.includes("x")) {
-                time *= 60 * args.duration.replace('x', '');
-            }
-            else {
-                time *= args.duration;
-            }
-            await badguy.timeout(time);
-        }
-        else {
-            await message.reply("user needs to be provided");
-        }
+		if (args.person != null) {
+			console.log(args.person)
+			const badguy = await message.guild.members.fetch(args.person.id)
+			let time = 1000 // ms
+			if (args.duration.includes('x')) {
+				time *= 60 * args.duration.replace('x', '')
+			} else {
+				time *= args.duration
+			}
+			await badguy.timeout(time)
+		} else {
+			await message.reply('user needs to be provided')
+		}
 	}
 }
